a7400bb14ada565093bafcd9141b8acedaf8a1c2
Sıla Er <98685874+silaver@users.noreply.github.com>
2022-04-16 15:30:16 +0300

    Time: 83 ms (5.14%), Space: 34.9 MB (26.95%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -1,3 +1,11 @@
+/*
+    SILA ER
+    ID: 20201701041
+    Time Complexity: O(n logn)
+    Space Complexity: O(n)
+
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -21,9 +29,12 @@ public:
         if (root->val > val)
             return searchBST(root->left, val);
         
-        return searchBST(root->right, val);
-        
-        
-        
+        return searchBST(root->right, val); 
     }
 };
+/*
+    STRATEGY
+    Simply check if the root is empty, if it is return null. If the value of the root is equal to val, return root. If the value of the node is bigger than val, search the left subtree. If it is bigger, search the right subtree.
+    TC is O(n logn) because no matter what, we are searching the half of the tree.
+    SC is O(n), n being the number of nodes in the tree.
+*/
