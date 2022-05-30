5609d9ad369da71536063583afc624dac40c9c58
Sıla Er <98685874+silaver@users.noreply.github.com>
2022-05-31 00:13:58 +0300

    Time: 29 ms (34.44%), Space: 8.8 MB (38.86%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int> mp;
-        for(int i = 0; i < magazine.size(); i++)
-            mp[magazine[i]]++;
-    
-        for(int i = 0; i < ransomNote.size(); i++){
-            if(mp[ransomNote[i]]>0)
-                mp[ransomNote[i]]--;
-
-            else 
+        
+        //declaring a hashmap
+        unordered_map<char, int> mp;
+        
+        //store all occurences of magazine in map
+        for(auto ch: magazine) 
+            mp[ch]++;
+        
+        //check all chars in ransomnote are present in map or not
+        for(auto ch: ransomNote ) 
+            mp[ch]--;
+        
+        //check for all chars of ransomNote in map if count is less than 0, means atleast one character not found in ransomNote
+        
+        for(auto ch: ransomNote ){
+            if(mp[ch]<0) 
                 return false;
-        }
+        } 
+        
+        //all characters are either 0 or greater than 0 after subtracting the frequency -> return true
         return true;
+    
     }
 };
