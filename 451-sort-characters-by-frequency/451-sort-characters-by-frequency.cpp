445718f48622669bf88f86bbad3274733cf5b25b
Sıla Er <98685874+silaver@users.noreply.github.com>
2022-05-31 00:48:09 +0300

    Time: 23 ms (23.40%), Space: 8.1 MB (85.37%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> freq;           //for frequency of characters
+        
+        priority_queue<pair<int,char>> maxheap; 
+        //maxheap according to frequency of characters
+        
+        for(char c: s)
+            freq[c]++;
+        
+        for(auto it: freq)
+            maxheap.push({it.second, it.first}); 
+        //heap will be constructed on the basis of frequency
+        
+        s = "";   
+        
+        while(!maxheap.empty()){
+            s += string(maxheap.top().first, maxheap.top().second); 
+            //frequency times the character
+            maxheap.pop();
+        }
+        return s;
+    }
+};
+/*
+    Time Complexity: O(n)
+    O(26log(n)) = O(log(n)) For Construction and extraction from heap
+    O(n) For storing the frequency in hashmap.
+*/
