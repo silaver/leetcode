3e3c717c140a9218be3b9eb8b9420fd9111395e5
Sıla Er <98685874+silaver@users.noreply.github.com>
2022-05-30 14:23:43 +0300

    Time: 15 ms (67.03%), Space: 21.7 MB (33.52%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -13,12 +13,15 @@ class Solution {
     TreeNode* prev = NULL;
 public:
     bool isValidBST(TreeNode* root) {
-        if(!root) return true;
+        if(!root) 
+            return true;
         
         if(!isValidBST(root->left))
             return false;
         
-        if(prev && prev->val >= root->val) return false;
+        if(prev && prev->val >= root->val) 
+            return false;
+        
         prev = root;
         
         if(!isValidBST(root->right))
